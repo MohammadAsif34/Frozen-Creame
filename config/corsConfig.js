0dcd264c50c41d1101a50f9a5568aae8c824ee37
MohammadAsif34 <shadowforever34@gmail.com>
2025-09-05 19:00:29 +0530

    update_url

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,6 +1,6 @@
 export const corsConfig = {
   origin: [
-    "https://frozencreameadmin.netlify.app",
+    "https://frozen-kreme-admin.netlify.app",
     "http://localhost:5173",
     "http://localhost:5174",
   ],
